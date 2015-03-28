4127b83ee4b86e40ac108c1ea502ed5e04aa5759
gwacter <gwacter@gmail.com>
2015-03-28 23:24:19 +1300

    Refactored into two functions, second function run_test() uses subprocess to spawn a subprocess that is communicated with using a pipe

--- traverse.py
+++ traverse.py
@@ -1,13 +1,34 @@
-# first, get it to find (in this case: assignment1.py)
-# second, print out the location (including the dirname)
-# ---- assume that the code to run is not in marking btw
+def run_test():
+    from subprocess import Popen, PIPE
 
-# Import the os module, for the os.walk function
-import os
- 
-# Set the directory you want to start from
-rootDir = '/home/ian/marking'
-for dirName, subdirList, fileList in os.walk(rootDir):
-    for fname in fileList:
-        if fname == "assignment1.py":
-        	print("{0}/{1}/{2}".format(dirName,subdirList,fname))
+    p = Popen(["ls", "-1"], stdout=PIPE)
+    output, err = p.communicate()
+    print("="*40)
+    print(output.decode(), end='')
+    print("="*40)
+
+def walk_submissions():
+    # first, get it to find (in this case: assignment1.py)
+    # second, print out the location (including the dirname)
+    # ---- assume that the code to run is not in marking btw
+
+    # assumes that have copied the assignment file to makring 
+    # subdirectory and that the output from running the marking
+    # scriptt will also be placed there
+
+    # Import the os module, for the os.walk function
+    import os
+
+    # Set the directory you want to start from
+    rootDir = '/home/ian/submit'
+    for dirpath, dirnames, fileList in os.walk(rootDir):
+        for fname in fileList:
+            if fname == "assignment1.py":
+                if "marking" in dirpath:
+        	        print("{0}/{1}".format(dirpath,fname))
+
+    run_test()
+
+
+if __name__ == "__main__":
+    walk_submissions()
